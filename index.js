63fb3aa66770686c81bfe49af70cfba629c81ed0
caseytansey <ctansey12600@outlook.com>
2021-08-19 21:06:58 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -47,4 +47,6 @@ const[indg] = colors.slice(5,6)
 const {muppetName, color, song, job, partner} = muppet
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
-const {song}
+const {song2, song4} = nestedMuppet.album.theMuppetMovie
+
+const {nestedJob, nestedPartner} = nestedMuppet
